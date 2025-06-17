9623eb33235909462db1d7dc4ca3a69f3118cf0a
Ankush Kumar <ankush.kumar@hertshtengroup.com>
2025-06-17 03:56:30 +0100

    Added GitHub and LinkedIn to footer

--- main.py
+++ main.py
@@ -16,3 +16,12 @@ df_target, df_softs = load_data(config)
 
 # --- UI Tabs ---
 display_tabs(config, df_target, df_softs)
+
+st.markdown("""
+    <hr style="margin-top: 50px;">
+    <div style='text-align: center; padding: 10px; font-size: 0.9em; color: grey;'>
+        Made with ❤️ by Ankush Kumar &nbsp;|&nbsp;
+        <a href='https://github.com/ankushkgpian' target='_blank'>GitHub</a> &nbsp;|&nbsp;
+        <a href='https://www.linkedin.com/in/ankushkgpian/' target='_blank'>LinkedIn</a>
+    </div>
+""", unsafe_allow_html=True)
